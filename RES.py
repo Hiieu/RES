df0f360ab3f9fee7527be20ccda6ebd213de6b62
hdoan <stealthpancakes@gmail.com>
2017-07-23 19:33:25 +0100

    [RES] Add application configuration and Report Model

--- RES.py
+++ RES.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=
+    'postgresql://interview:LetMeIn@candidate.suade.org/suade',
+    SQLALCHEMY_ECHO=False,
+    SECRET_KEY='A3ZZpa&Z&*kP88KhWYrf3',
+    DEBUG=True,
+)
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Report(db.Model):
+    __tablename__ = 'reports'
+    id = db.Column('id', db.Integer, primary_key=True)
+    data = db.Column(db.Text)
 
 
 if __name__ == '__main__':
